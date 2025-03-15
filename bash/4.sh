44410458fabf97e1b228d3477f8a5623a4550978
Sofia <sofia.kalinina.04.09@mail.ru>
2025-03-15 16:54:18 +0000

    bash

--- bash/4.sh
+++ bash/4.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Проверка на наличие аргумента
 if [ $# -eq 0 ]; then
     echo "Unknown planet"
     exit 1
+fi
 
+# Ассоциативный массив с количеством спутников для каждой планеты
 declare -A planets=(
     ["Mercury"]=0
     ["Venus"]=0
@@ -15,10 +18,13 @@ declare -A planets=(
     ["Neptune"]=14
 )
 
+# Получение названия планеты из аргумента
 planet="$1"
 
+# Проверка, существует ли планета в массиве
 if [[ -z "${planets[$planet]}" ]]; then
     echo "Unknown planet"
 else
+    # Вывод количества спутников
     echo "${planets[$planet]}"
 fi
